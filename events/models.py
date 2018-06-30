0157998c98ff621500678e69605151a8cd75bf3b
matthewrr <matthewrr@users.noreply.github.com>
2018-06-30 21:00:34 +0000

    Initial commit

---
+++ events/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# class Event(models.Model
+#     event_id = pass
+#     title = pass
+#     doors_open = pass
+#     employees = pass
---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# class User(models.Model):
+#     name = pass
+#     phone_number = pass
+#     employee_id = pass
+#     active = pass
+#     food_permit = pass
+#     alcohol_permit = pass
+#     admin = pass
