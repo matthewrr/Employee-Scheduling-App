3907d8d1be7bce5d72641a2933ae507b36c0fae6
matthewrr <matthewrr@users.noreply.github.com>
2018-07-03 22:00:14 +0000

    Add Select2 for searchable employee dropdown

--- events/static/events/js/script.js
+++ events/static/events/js/script.js
@@ -1,4 +1,7 @@
 // In your Javascript (external .js resource or <script> tag)
 $(document).ready(function() {
-    $('.js-example-basic-single').select2();
+    $(".js-example-basic-single").select2({
+        placeholder: "Select Employee",
+        allowClear: true
+    });
 });
