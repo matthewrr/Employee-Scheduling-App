7d2f8081ece1a7198bcb52d3b03d1aff2b734cfb
matthewrr <matthewrr@users.noreply.github.com>
2018-07-03 16:42:00 +0000

    Add manager and prep positions to Location model

--- locations/models.py
+++ locations/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Location(models.Model):
     location_id = models.CharField(max_length=256)
     title = models.CharField(max_length=256)
-    positions = models.IntegerField(default=6)
+    total_positions = models.IntegerField(default=6)
+    manager_position = models.BooleanField(default=False)
+    prep_position = models.BooleanField(default=False)
     
     def __str__(self):
         return self.title
