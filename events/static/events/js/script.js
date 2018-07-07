9005176a3c798d308182514ced040c5a0bcca99a
matthewrr <matthewrr@users.noreply.github.com>
2018-07-07 23:57:18 +0000

    Add search to Location app

--- events/static/events/js/script.js
+++ events/static/events/js/script.js
@@ -27,16 +27,38 @@ function myFunction() {
   }
 }
 
+function myFunction() {
+    
+  var input, filter, table, tr, td, td2, i;
+  input = document.getElementById("locationSearch");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("locationTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    td2 = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+
 $(function() {
   $('.add-employee').click(function(){
     var myparent = $(this).parent().prop('className');
+    
     var newRow = document.getElementById("new-employee");
     $('#' + myparent).append(newRow)
+    // $('#' + myparent).append("<div class='row'><div class='col col-12'><input type='text' class='form-control float-left' placeholder='POS:' style='width:20%;'><input type='text' class='form-control float-right' placeholder='Employee Name' style='width:80%;'></div></div>");
   });
 });
 
-
-$(document).ready(function() {
+$(function() {
   $('.remove-employee').click(function(){
     var target = $(this).attr('id')
     var els = document.getElementsByClassName(target);
--- static/events/js/script.js
+++ static/events/js/script.js
@@ -6,12 +6,33 @@ $(document).ready(function() {
     });
 });
 
+// function myFunction() {
+    
+//   var input, filter, table, tr, td, td2, i;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   table = document.getElementById("myTable");
+//   tr = table.getElementsByTagName("tr");
+
+//   for (i = 0; i < tr.length; i++) {
+//     td = tr[i].getElementsByTagName("td")[1];
+//     td2 = tr[i].getElementsByTagName("td")[2];
+//     if (td) {
+//       if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//         tr[i].style.display = "";
+//       } else {
+//         tr[i].style.display = "none";
+//       }
+//     } 
+//   }
+// }
+
 function myFunction() {
     
   var input, filter, table, tr, td, td2, i;
-  input = document.getElementById("myInput");
+  input = document.getElementById("locationSearch");
   filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
+  table = document.getElementById("locationTable");
   tr = table.getElementsByTagName("tr");
 
   for (i = 0; i < tr.length; i++) {
