1d5c85e1d3231440d3ca4ea3186e2902082d8b67
matthewrr <matthewrr@users.noreply.github.com>
2018-08-22 03:18:35 +0000

    Add 'created' and 'modified' timestamps to Schedule model

--- schedules/models.py
+++ schedules/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from jsonfield import JSONField
 from events.models import Event
+from django.utils import timezone
 
 class Schedule(models.Model):
-    title = models.CharField(max_length=256)
-    template = models.BooleanField(default=False)
-    roster = JSONField(blank=True,null=True)
-    event = models.OneToOneField(Event, on_delete=models.CASCADE,null=True,blank=True)
-    scheduled = models.IntegerField(null=True,blank=True)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+    title        = models.CharField(max_length=256)
+    template     = models.BooleanField(default=False)
+    roster       = JSONField(blank=True,null=True)
+    event        = models.OneToOneField(Event, on_delete=models.CASCADE,null=True,blank=True)
+    scheduled    = models.IntegerField(null=True,blank=True)
+    created      = models.DateTimeField(editable=False)
+    modified     = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Schedule, self).save(*args, **kwargs)
     
     # def name_schedule(self):
     #     if not self.title:
