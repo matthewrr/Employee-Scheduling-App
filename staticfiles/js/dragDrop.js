65065773af41f4879a424bb1619a55e459382bab
matthewrr <matthewrr@users.noreply.github.com>
2018-09-13 22:55:05 +0000

    Finalize Bootstrap-less grid design with four columns

--- staticfiles/js/dragDrop.js
+++ staticfiles/js/dragDrop.js
@@ -43,7 +43,7 @@ itemContainers.forEach(function (container) {
           // The layout's total height.
           height: 0,
           // Should Muuri set the grid's width after layout?
-          setWidth: true,
+          setWidth: false,
           // Should Muuri set the grid's height after layout?
           setHeight: true
         };
@@ -53,7 +53,7 @@ itemContainers.forEach(function (container) {
       var m;
       var x = 0;
       var y = 0;
-      z = 0;
+      var z = 0;
       var w = 0;
       var h = 0;
       for (var i = 0; i < items.length; i++) {
@@ -80,7 +80,8 @@ itemContainers.forEach(function (container) {
   
       // Calculate the layout's total width and height. 
       // layout.width = w + x;
-      layout.width = w * 4;
+      layout.width = 250;
+      console.log(layout.width)
       layout.height = y + h;
   
       return layout;
@@ -164,98 +165,48 @@ boardGrid = new Muuri('.board', {
   },
   dragReleaseDuration: 400,
   dragReleaseEasing: 'ease',
-  layout: function (items, gridWidth, gridHeight) {
-    // The layout data object. Muuri will read this data and position the items
-    // based on it.
-    
-    var layout = {
-      // The layout item slots (left/top coordinates).
-      slots: [],
-      // The layout's total width.
-      width: 0,
-      // The layout's total height.
-      height: 0,
-      // Should Muuri set the grid's width after layout?
-      setWidth: true,
-      // Should Muuri set the grid's height after layout?
-      setHeight: true
-    };
+  // layout: function (items, gridWidth, gridHeight) {
+  //   // The layout data object. Muuri will read this data and position the items
+  //   // based on it.
+  //   console.log(gridWidth)
+  //   var layout = {
+  //     // The layout item slots (left/top coordinates).
+  //     slots: [],
+  //     // The layout's total width.
+  //     width: 0,
+  //     // The layout's total height.
+  //     height: 0,
+  //     // Should Muuri set the grid's width after layout?
+  //     setWidth: true,
+  //     // Should Muuri set the grid's height after layout?
+  //     setHeight: true
+  //   };
  
-    // Calculate the slots.
-    var item;
-    var m;
-    var x = 0;
-    var y = 0;
-    var z = 0;
-    var w = 0;
-    var h = 0;
-    var rowHeights = [];
-    var columns = 4;
-    var colObjs = 0;
-    var colHeight = 0;
-    var count = 0
-    for (var i = 0; i < items.length; i++) {
-      
-      
-      
-      // y += h;
-      // y = 0
-      // w = item.getWidth() + m.left + m.right;
-      // h = item.getHeight() + m.top + m.bottom;
-      
-      item = items[i];
-      m = item.getMargin();
-      w = item.getWidth();
-      h = item.getHeight() + m.top + m.bottom;
-      
-      
-      var column = count % 4;
-      if (column === 0) {
-        
-        console.log('column is 0')
-        
-        var maxHeight = Math.max(...rowHeights.slice(-4));
-        var currentHeight = rowHeights
-        const arrSum = arr => arr.reduce((a,b) => a + b, 0)
-        var heightArr = rowHeights.slice(colObjs*-1);
-        for (var height in heightArr) {
-          colHeight += Number(height);
-          console.log(heightArr)
-          console.log(colHeight);
-          
-          if (h < maxHeight) {
-            colObjs += 1;
-            y = maxHeight;
-            x = x
-            
-          } else {
-            colObjs = 1
-            
-            y = maxHeight
-            x = (w/5)*i
-            
-          }
-          console.log('colobjs is: ' + colObjs)
-        }
-      }
-      rowHeights.push(h)
-  
-      // x = (w/5)*i
-      layout.slots.push(x, y);
-      colHeight = 0
-      // x += ;
-    }
-    // Calculate the layout's total width and height. 
-    // layout.width = x + w;
-    // layout.width = (layout.slots[2] - layout.slots[1]) * 4;
-    layout.width = 16 + '%'
-    layout.height = y + h;
+  //   // Calculate the slots.
+  // var item;
+  //   var m;
+  //   var x = 0;
+  //   var y = 0;
+  //   var w = 0;
+  //   var h = 0;
+  //   for (var i = 0; i < items.length; i++) {
+  //     item = items[i];
+  //     x += w;
+  //     y += h;
+  //     m = item.getMargin();
+  //     w = item.getWidth() + m.left + m.right;
+  //     h = item.getHeight() + m.top + m.bottom;
+  //     layout.slots.push(x, y);
+  //   }
 
-    return layout;
-  }
+  //   // Calculate the layout's total width and height. 
+  //   // layout.width = x + w;
+  //   layout.width = 400;
+  //   layout.height = y + h;
+  //   console.log(gridWidth)
+  //   return layout;
+  // }
 });
-
-
 
 var employeeContainers = [].slice.call(document.querySelectorAll('.employee-column-content'));
 employeeContainers.forEach(function (container) {
@@ -289,62 +240,3 @@ employeeContainers.forEach(function (container) {
   columnGrids.push(employeeGrid)
 
 });
-
-
-
-
-var rowObjCounter = 0;
-
-for (obj in objs) {
-  var checkNewRow = rowObjCounter % 4 === 0;
-  if (checkNewRow) {
-    var objHeight = ;
-    var currentRowHeight = ;
-    var maxRowHeight = ;
-    if (currentRowHeight + objHeight < maxRowHeight) {
-      x += 0
-      y = currentRowHeight;
-
-    } else {
-      x = 0
-      y = maxRowHeight;
-    }
-  }
-}
-
-var desiredCols = 4;
-var maxRowObj = ;
-var numOfRowColumns = ;
-var rowColHeights = []
-
-var itemHeight = ;
-var maxRowHeight = ;
-
-
-if (numOfRowColumns === desiredCols) {
-  if (itemHeight + rowColHeights[-1] > maxRowHeight) {
-    // start new row
-  } else {
-    // append to 
-  }
-}
-
-
-New Row?column
-If current item + current row column height > max row height
-X = 0
-Y = max row height
- Row columns at 0
-Row column height = item height
-Same Row
-If current item + current row column height <= max row height
-X = most recent x
-Y = most reent row column height
-Row column height += current item height
-Row columns unchanged
-
-Else (you’ll start a new column)...
-Row columns += 1
-X = most recent x in list + item width
-Y = most recent y
-rowcolumnheight  = item height
